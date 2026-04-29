99db44ec3d98642838daae013da225badd7783e0
Matthew Vilaysack <m_vilaysack@apple.com>
2026-04-29 14:57:33 -0700

    first commit

---
+++ break-reminder.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Reminds you to take a break every 50 minutes via macOS notification + terminal bell.
+
+INTERVAL=$((50 * 60))
+
+echo "Break reminder started. You'll be notified every 50 minutes. Press Ctrl+C to stop."
+
+while true; do
+  sleep "$INTERVAL"
+  osascript -e 'display notification "Time to stand up, stretch, and rest your eyes!" with title "Break Time 🧘" sound name "Glass"'
+  echo -e "\a[$(date '+%H:%M')] Take a break!"
+done
